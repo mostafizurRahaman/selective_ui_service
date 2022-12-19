5e09d4793e106c102a2b9eca9e685b9691e5b0d2
mostafizurRahaman <fahim654326@gmail.com>
2022-12-20 02:46:25 +0600

    selection data added on dropdown selection menu

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4nkvsmn.mongodb.net/?retryWrites=true&w=majority`;
+
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
 try{
-      
+
 }
 finally{
    
